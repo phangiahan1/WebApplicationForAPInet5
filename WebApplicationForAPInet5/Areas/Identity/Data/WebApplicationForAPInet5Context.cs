02e0844287fd7ac2d23e049ce2b04fe02bab4bf9
Phan Gia Han <phanhan226@gmail.com>
2022-04-20 13:45:40 +0700

    Add project files.

---
+++ WebApplicationForAPInet5/Areas/Identity/Data/WebApplicationForAPInet5Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationForAPInet5.Data
+{
+    public class WebApplicationForAPInet5Context : IdentityDbContext<IdentityUser>
+    {
+        public WebApplicationForAPInet5Context(DbContextOptions<WebApplicationForAPInet5Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
